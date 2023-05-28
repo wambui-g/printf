fb251d162b0937b51d638225581897cf48dcae6e
“Aechybella” <berylevelyn96@gmail.com>
2023-05-28 06:59:03 -0700

    printf

--- get_flags_function.c
+++ get_flags_function.c
@@ -2,7 +2,7 @@
 
 /**
  * get_flags - Calculates active flags
- * @format: Formatted string in which to print the arguments
+ * @format: Formats the string in which to print arguments are printed
  * @i: take a parameter.
  * Return: Flags:
  */
@@ -12,8 +12,8 @@ int get_flags(const char *format, int *i)
 	/* 1 2 4 8  16 */
 	int j, curr_i;
 	int flags = 0;
-	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
 	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
 
 	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 	{
