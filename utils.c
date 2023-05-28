f9cb2a21461cbf958837b7b2710e0d8c211e19b0
wambui-g <grayciekamau03@gmail.com>
2023-05-28 13:08:18 +0000

    functions

--- utils.c
+++ utils.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * is_printable - Evaluates if a char is printable
  * @c: Char to be evaluated.
@@ -11,6 +12,7 @@ int is_printable(char c)
 
 	return (0);
 }
+
 /**
  * append_hexa_code - Append ascci in hexadecimal code to buffer
  * @buffer: Array of chars.
@@ -33,6 +35,7 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 
 	return (3);
 }
+
 /**
  * is_digit - Verifies if a char is a digit
  * @c: Char to be evaluated
@@ -45,6 +48,7 @@ int is_digit(char c)
 
 	return (0);
 }
+
 /**
  * convert_size_number - Casts a number to the specified size
  * @num: Number to be casted.
@@ -60,6 +64,7 @@ long int convert_size_number(long int num, int size)
 
 	return ((int)num);
 }
+
 /**
  * convert_size_unsgnd - Casts a number to the specified size
  * @num: Number to be casted
