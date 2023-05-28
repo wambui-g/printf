86827493f960773ab48736914554e9160bf7a886
wambui-g <grayciekamau03@gmail.com>
2023-05-28 10:33:24 +0000

    _printf

--- main.h
+++ main.h
@@ -1,60 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H
+#define _MAIN_H
 
 #include <stdarg.h>
 
-/**
- * enum flag_t - flag types
- * @FLAG_SIGN: sign
- * @FLAG_BLANK: space
- * @FLAG_ALT: hex
- * @FLAG_LEFT: left
- * @FLAG_ZERO: zero
- * @FLAG_LONG: length
- * @FLAG_SHORT: short
- * @FLAG_PRECISION: precision
- *
- */
-typedef enum flag_t
-{
-	FLAG_PRECISION = 1 << 7
-	FLAG_BLANK = 1 << 1,
-	FLAG_ALT = 1 << 2,
-	FLAG_LEFT = 1 << 3,
-        FLAG_SIGN = 1,
-	FLAG_ZERO = 1 << 4,
-	FLAG_LONG = 1 << 5,
-	FLAG_SHORT = 1 << 6,
-} flag_t;
-
-/**
- * struct specifier_t - contains specifiers for printf
- *
- * @width: width of field to print
- * @precision: precision of field to print
- * @flags: flags set
- * @specifier: the data type to print, c, s, d, x, etc.
- */
-typedef struct specifier_t
-{
-	unsigned int width;
-	unsigned int precision;
-	char specifier;
-	unsigned int flags;
-} specifier_t;
-
-/**
- * struct format_t - Format
- * @specifier: character
- * @print: print function for specifier
- *
- */
-typedef struct format_t
-{
-	int (*print)(specifier_t *spec, va_list arg);
-	char specifier;
-} format_t;
-
 int _printf(const char *format, ...);
 
-#endif
+#endif /* MAIN_H */
