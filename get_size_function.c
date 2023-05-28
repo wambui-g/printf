c2c8201200b0153fc35fa1ad09548e3110faf813
“Aechybella” <berylevelyn96@gmail.com>
2023-05-28 09:28:35 -0700

    printf

--- get_size_function.c
+++ get_size_function.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-get_size - Calculates the size required for argument casting based on the format specifier.
-@format: The formatted string containing the argument.
-@i: The index of the current argument.
-
-Return: The calculated size for argument casting.
-*/
+ * get_size - Calculates the size to cast the argument
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed.
+ *
+ * Return: Precision.
+ */
 int get_size(const char *format, int *i)
 {
 	int curr_i = *i + 1;
