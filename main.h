76795785652fcd520c90a5a9b58b66fa3e4c8b7a
“Aechybella” <berylevelyn96@gmail.com>
2023-05-28 05:20:13 -0700

    printf

--- main.h
+++ main.h
@@ -97,21 +97,21 @@ int handle_write_char(char c, char buffer[],
 		int flags, int width, int precision, int size);
 int write_number(int is_positive, int ind, char buffer[],
 		int flags, int width, int precision, int size);
-int write_num(int ind, char bff[], int flags, int width, int precision,
-		int length, char padd, char extra_c);
 int write_pointer(char buffer[], int ind, int length,
 		int width, int flags, char padd, char extra_c, int padd_start);
 
 int write_unsgnd(int is_negative, int ind,
 		char buffer[],
 		int flags, int width, int precision, int size);
+int write_num(int ind, char bff[], int flags, int width, int precision,
+                int length, char padd, char extra_c);
 
 /****************** UTILS ******************/
+long int convert_size_number(long int num, int size);
+long int convert_size_unsgnd(unsigned long int num, int size);
+
 int is_printable(char);
 int append_hexa_code(char, char[], int);
 int is_digit(char);
-
-long int convert_size_number(long int num, int size);
-long int convert_size_unsgnd(unsigned long int num, int size);
 
 #endif /* MAIN_H */
